b08ddaa75ee76f9e92aa1df00df0bc250db4c42c
AdolfoVV <sheldoncttbs@gmail.com>
2020-11-03 21:13:40 -0500

    Toast.

--- index.php
+++ index.php
@@ -26,6 +26,20 @@
       </div>
     </div>
 
+    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+  <div class="toast-header">
+    
+    <strong class="mr-auto">Bootstrap</strong>
+    <small class="text-muted">11 mins ago</small>
+    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <div class="toast-body">
+    Hello, world! This is a toast message.
+  </div>
+</div>
+
   <input class="btn btn-primary" type="submit" id ="boton" hidden>
 </form>
 
